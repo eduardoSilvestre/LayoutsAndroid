217b9360b53b7825d61066c751c606a95fa87092
Eduardo Silvestre <eduardo_mizael@outlook.com.br>
2019-06-03 11:59:21 -0300

    Finalizado

---
+++ app/src/main/java/br/com/digitalhouse/constraintlayout/LoginDigitalHouse.java
@@ -0,0 +1,13 @@
+package br.com.digitalhouse.constraintlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginDigitalHouse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_digital_house);
+    }
+}
